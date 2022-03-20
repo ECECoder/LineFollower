129776116cc3f6af572be5c9bdd32b08fc310dc5
Stuart <smccutchen74@gmail.com>
2022-03-20 19:02:44 -0400

    FasterBot

--- JackiFSMmain.c
+++ JackiFSMmain.c
@@ -38,14 +38,14 @@ typedef const struct State State_t;
 #define R_Lost &fsm[7]
 #define Stop   &fsm[8]
 State_t fsm[9]={
-  {4000, 4000,  10, 0, { L_Center, Left, H_left, Right, H_right, L_Lost }},   // Left Center
-  {4000, 4000,  10, 0, { R_Center, Left, H_left, Right, H_right, R_Lost }},   // Right Center
-  {4000, 3000, 100, 0, { L_Center, Left, H_left, Right, H_right, L_Center }},  // Left of line (turn right)
-  {2500, 2500, 100, 1, { L_Center, Left, H_left, Right, H_right, L_Lost }},  // H_left of line (turn hard right)
-  {3000, 4000, 100, 0, { R_Center, Left, H_left, Right, H_right, R_Center }},  // Right of line (turn left)
-  {2500, 2500, 100, 2, { R_Center, Left, H_left, Right, H_right, R_Lost }},  // H_right of line (turn hard left)
-  {3500, 3500, 100, 1, { L_Center, Left, H_left, Right, H_right, L_Lost }},  // Left lost
-  {3500, 3500, 100, 2, { R_Center, Left, H_left, Right, H_right, R_Lost }},  // Right lost
+  {6000, 6000,  20, 0, { L_Center, Left, H_left, Right, H_right, L_Lost }},   // Left Center
+  {6000, 6000,  20, 0, { R_Center, Left, H_left, Right, H_right, R_Lost }},   // Right Center
+  {6000, 5000,  60, 0, { L_Center, Left, H_left, Right, H_right, L_Center }},  // Left of line (turn right)
+  {3500, 1500,  60, 1, { L_Center, Left, H_left, Right, H_right, L_Lost }},  // H_left of line (turn hard right)
+  {5000, 5000,  60, 0, { R_Center, Left, H_left, Right, H_right, R_Center }},  // Right of line (turn left)
+  {1500, 3500,  60, 2, { R_Center, Left, H_left, Right, H_right, R_Lost }},  // H_right of line (turn hard left)
+  {4000, 3500, 100, 1, { L_Center, Left, H_left, Right, H_right, L_Lost }},  // Left lost
+  {3500, 4000, 100, 2, { R_Center, Left, H_left, Right, H_right, R_Lost }},  // Right lost
   {   0,    0, 500, 0, { Stop,   Stop, Stop,  Stop, Stop, Stop }}   // Stop
 };
 
